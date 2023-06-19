c44c2aeea60d960d3572419a50f4d89e0e26f270
GOWTHAM M H <117714418+MHGOWTHAM@users.noreply.github.com>
2023-06-19 23:30:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function letterCombinations(input_digit) {
-  //Complete the function
+  Given a digit string, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below.
+
+2=>abc 3=>def 4=>ghi 5=>jkl 6=>mno 7=>pqrs 8=>tuv 9=>wxyz The digit 0 maps to 0 itself. The digit 1 maps to 1 itself.
+
+Input: Digit string "23"
+
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 }
 
 module.exports = letterCombinations;
